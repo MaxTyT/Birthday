182ae61550422f7804611e499f8226464b5e81f3
MaxTyT <pdaz@mail.ru>
2013-03-15 22:10:08 +0300

    New

---
+++ src/org/birthday/BirthdayActivity.java
@@ -0,0 +1,13 @@
+package org.birthday;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class BirthdayActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
